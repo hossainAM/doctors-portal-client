3b9988f5ba82e02c500bf8393af22042c4df385c
Amir Hossain <amir508_du@yahoo.com>
2022-05-15 04:24:00 +0600

    login and signup page created

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,9 +2,32 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            <h1>This is login page</h1>
-        </div>
+       <div class="flex h-[calc(100vh-4rem)] justify-center items-center">
+            <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="card-body">
+                <h1 class="text-2xl text-center text-neutral">Login</h1>
+                <div class="form-control w-full max-w-xs">
+                    <label class="label">
+                        <span class="label-text text-accent font-semibold">Email</span>
+                    </label>
+                    <input type="text" class="input input-bordered w-full max-w-xs mb-2" />
+                    <label class="label">
+                        <span class="label-text text-accent font-semibold">Password</span>
+                    </label>
+                    <input type="text" class="input input-bordered w-full max-w-xs" />
+                    <label class="label mb-2">
+                        <span class="label-text-alt text-accent font-semibold">Forgot Password?</span>
+                    </label>
+                    <input type="Submit" class="input input-bordered w-full max-w-xs bg-accent text-white" value="LOGIN"/>
+                    <label class="label text-accent font-semibold">
+                        <span class="label-text-alt text-accent font-semibold">New to Doctors Portal?</span><span class="text-primary"><small>Create new account</small></span>
+                    </label>
+                     <div class="divider">OR</div>
+                     <input type="text" placeholder="CONTINUE WITH GOOGLE" class="input text-center input-bordered input-accent w-full max-w-xs" />
+                </div>
+            </div>
+            </div> 
+       </div>
     );
 };
 
--- src/pages/Login/SignUp.js
+++ src/pages/Login/SignUp.js
@@ -3,7 +3,36 @@ import React from 'react';
 const SignUp = () => {
     return (
         <div>
-            <h1>This is sign up page</h1>
+            <div class="flex h-[calc(100vh-4rem)] justify-center items-center">
+            <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="card-body">
+                <h1 class="text-2xl text-center text-neutral">Sign Up</h1>
+                <div class="form-control w-full max-w-xs">
+                    <label class="label">
+                        <span class="label-text text-accent font-semibold">Name</span>
+                    </label>
+                    <input type="text" class="input input-bordered w-full max-w-xs mb-2" />
+                    <label class="label">
+                        <span class="label-text text-accent font-semibold">Email</span>
+                    </label>
+                    <input type="email" class="input input-bordered w-full max-w-xs mb-2" />
+                    <label class="label">
+                        <span class="label-text text-accent font-semibold">Password</span>
+                    </label>
+                    <input type="text" class="input input-bordered w-full max-w-xs" />
+                    <label class="label mb-2">
+                        <span class="label-text-alt text-accent font-semibold">Forgot Password?</span>
+                    </label>
+                    <input type="Submit" class="input input-bordered w-full max-w-xs bg-accent text-white" value="SIGN UP"/>
+                    <label class="label text-accent font-semibold">
+                        <span class="label-text-alt text-accent font-semibold">Already have an account?</span><span class="text-primary"><small>Login here</small></span>
+                    </label>
+                     <div class="divider">OR</div>
+                     <input type="text" placeholder="CONTINUE WITH GOOGLE" class="input text-center input-bordered input-accent w-full max-w-xs" />
+                </div>
+            </div>
+            </div> 
+       </div>
         </div>
     );
 };
